951d00fc086ad35d388d9d9c423b795786bcaee2
Tom Spencer <tom.spencer@studiozeffa.com>
2018-08-03 14:09:40 +0100

    Support slider responding to press

--- src/Slider.js
+++ src/Slider.js
@@ -167,6 +167,11 @@ export default class Slider extends PureComponent {
      * Used to configure the animation parameters.  These are the same parameters in the Animated library.
      */
     animationConfig: PropTypes.object,
+
+    /**
+      * Set to true to update the value whilst pressing the Slider
+      */
+    trackPressable : PropTypes.bool,
   };
 
   static defaultProps = {
@@ -180,6 +185,7 @@ export default class Slider extends PureComponent {
     thumbTouchSize: { width: 40, height: 40 },
     debugTouchArea: false,
     animationType: 'timing',
+    trackPressable: false,
   };
 
   state = {
@@ -322,6 +328,7 @@ export default class Slider extends PureComponent {
       style,
       trackStyle,
       thumbStyle,
+      trackPressable,
       ...otherProps
     } = props;
 
@@ -332,15 +339,15 @@ export default class Slider extends PureComponent {
     e: Object /* gestureState: Object */,
   ): boolean =>
     // Should we become active when the user presses down on the thumb?
-    this._thumbHitTest(e);
+    this.props.trackPressable || this._thumbHitTest(e);
 
   _handleMoveShouldSetPanResponder(/* e: Object, gestureState: Object */): boolean {
     // Should we become active when the user moves a touch over the thumb?
     return false;
   }
 
-  _handlePanResponderGrant = (/* e: Object, gestureState: Object */) => {
-    this._previousLeft = this._getThumbLeft(this._getCurrentValue());
+  _handlePanResponderGrant = (e: Object/*, gestureState: Object */) => {
+    this._previousLeft = this.props.trackPressable ? e.nativeEvent.locationX - (this.props.thumbTouchSize.width/2) : this._getThumbLeft(this._getCurrentValue());
     this._fireChangeEvent('onSlidingStart');
   };
 
